2229e587d6897228476af6348ccb48e3b2463a30
erickprogramer <115942865+ErickProgramer@users.noreply.github.com>
2023-09-19 15:24:57 -0300

    auxiliar documentation added

--- auxiliar.lua
+++ auxiliar.lua
@@ -1,9 +1,17 @@
 local aux = {}
 
+--- expands the `type` function
+--- if the type of `x` is a number, he return float or integer,
+--- else return: `type(x)`
+---@param x any
+---@return string
 function aux.type_pp(x)
     if math.type(x) ~= "nil" then return math.type(x) else return type(x) end
 end
 
+--- checks if the `parameter` is of type `parameter_expected`
+--- if it is not, it displays an error message on the screen with all the necessary information
+--- if `parameter_expected` is `"number"` this means that `parameter` can be float or integer
 ---@param func_name string
 ---@param parameter any
 ---@param parameter_expected string
@@ -16,6 +24,7 @@ function aux.expected_arg(func_name, parameter, parameter_expected, argument)
     end
 end
 
+--- works like `expected_arg`, but allowing you to check more than one parameter at once
 ---@param func_name string
 ---@param parameters table
 ---@param parameters_expected table
